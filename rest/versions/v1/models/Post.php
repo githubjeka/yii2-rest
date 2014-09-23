45f284f637959d395638c2b1fe1a347d93f7ea1a
githubjeka <et.coder@gmail.com>
2014-09-23 14:56:27 +0300

    change delete method

--- rest/versions/v1/models/Post.php
+++ rest/versions/v1/models/Post.php
@@ -6,5 +6,22 @@ use\common\models\Post as CommonPost;
 
 class Post extends CommonPost
 {
+    const STATUS_DELETED = 3;
 
+    public function delete()
+    {
+        $result = false;
+        if ($this->beforeDelete()) {
+
+            $result = $this->updateAttributes(['status' => self::STATUS_DELETED]);
+            $this->afterDelete();
+        };
+
+        return $result;
+    }
+
+    public static function find()
+    {
+        return parent::find()->where('status <>' . self::STATUS_DELETED);
+    }
 }
