2f3c2a172f3e37efc135072a54eed2b214e821e4
githubjeka <et.coder@gmail.com>
2014-09-30 12:10:56 +0300

    add filtering by title

--- rest/versions/v1/controllers/PostController.php
+++ rest/versions/v1/controllers/PostController.php
@@ -1,6 +1,8 @@
 <?php
 namespace rest\versions\v1\controllers;
 
+use rest\versions\v1\models\Post;
+use yii\data\ActiveDataProvider;
 use yii\filters\auth\QueryParamAuth;
 use yii\rest\ActiveController;
 
@@ -15,5 +17,32 @@ class PostController extends ActiveController
             'class' => QueryParamAuth::className(),
         ];
         return $behaviors;
+    }
+
+    public function actions()
+    {
+        return array_merge(
+            parent::actions(),
+            [
+                'index' => [
+                    'class' => 'yii\rest\IndexAction',
+                    'modelClass' => $this->modelClass,
+                    'checkAccess' => [$this, 'checkAccess'],
+                    'prepareDataProvider' => function ($action) {
+                        /* @var $model Post */
+                        $model = new $this->modelClass;
+
+                        $query = $model::find();
+
+                        $dataProvider = new ActiveDataProvider(['query' => $query,]);
+
+                        $model->setAttribute('title', @$_GET['title']);
+                        $query->andFilterWhere(['like', 'title', $model->title]);
+
+                        return $dataProvider;
+                    }
+                ]
+            ]
+        );
     }
 }
