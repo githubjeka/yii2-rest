abd4314f1f68d4de10287323afc19fd5a3cd24b3
githubjeka <et.coder@gmail.com>
2015-01-19 11:46:12 +0300

    Fix issue #2
    Replace frontend alias to backend.

--- common/config/aliases.php
+++
@@ -1,5 +0,0 @@
-<?php
-Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('rest', dirname(dirname(__DIR__)) . '/rest');
-Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
